c84413f40e38ea70e8ef91de3ad272077b92ec1c
Hiếu Bùi <buihieu028@gmail.com>
2021-02-23 20:32:33 +0700

    update/api InnDetail
    Bổ sung thêm trường room của mỗi inn tại response trả về

--- api/app/Http/Controllers/Inn/InnController.php
+++ api/app/Http/Controllers/Inn/InnController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers\Inn;
 
 use App\Http\Requests\Inn\DetailInnRequest;
 use App\Http\Requests\Inn\InnDetailRequest;
+use App\Http\Resources\InnDetailResource;
+use App\Models\Inn;
 use App\Repositories\Inn\InnRepository;
 use App\Repositories\Inn\InnRepositoryInterface;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller as BaseController;
+use Illuminate\Validation\Rules\In;
 
 class InnController extends BaseController
 {
@@ -28,12 +31,13 @@ class InnController extends BaseController
 
     public function getDetailInn(Request $request, $id)
     {
-        
-        $inn = $this->innRepository->find($id);
+
+        $inn = $this->innRepository->with('rooms')->find($id);
+
         if (!$inn) {
             return response()->json(null, 404);
         }
-        return response()->json($inn, 200);
+        return response()->json(new InnDetailResource($inn), 200);
     }
 
 
