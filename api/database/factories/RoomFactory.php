3c75df907218f78ac1f30ebf1c1f3f89209eb02c
Hiếu Bùi <buihieu028@gmail.com>
2021-02-23 20:34:25 +0700

    add/RoomTableSeeder
    Bổ sung các lệnh để fake data cho bảng RoomTable

---
+++ api/database/factories/RoomFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(\App\Models\Room::class, function (Faker $faker) {
+    return [
+        //
+        'id' => $faker->unique()->numberBetween(1, 30),
+        'title' => $faker->text,
+        'inn_id' => \App\Models\Inn::all()->random()->id,
+        'room_type_id' => \App\Models\MstRoomType::all()->random()->id,
+        'price' => $faker->randomDigit,
+        'acreage' => $faker->randomDigit,
+        'description' => $faker->text,
+        'verified' => true,
+        'verified_at' => \Carbon\Carbon::now()->format('Y-m-d H:i:s'),
+        'available' => 1,
+        'status' => $faker->randomDigit,
+        'created_at' => \Carbon\Carbon::now()->format('Y-m-d H:i:s'),
+
+    ];
+});
