ba327c99b2fbbfee296eac70ac4b413fddbc414f
Hiếu Bùi <buihieu028@gmail.com>
2021-02-25 16:08:02 +0700

    Update InnDetailForRoomResource.php
    Sửa lại display thời gian
    
    Nếu hour và minute < 10 thì thêm số 0 vào trước nó

--- api/app/Http/Resources/InnDetailForRoomResource.php
+++ api/app/Http/Resources/InnDetailForRoomResource.php
@@ -21,8 +21,8 @@ class InnDetailForRoomResource extends JsonResource
             'owner_contact' => $this->getOwnerContact(),
             'water_price' => $this->water_price,
             'electric_price' => $this->electric_price,
-            'open_time' => $this->open_hour . ":" . $this->open_minute,
-            'close_time' => $this->close_hour . ":" . $this->close_minute,
+            'open_time' => (($this->open_hour) < 10 ? "0" . $this->open_hour : $this->open_hour) . ":" . (($this->open_minute) < 10 ? "0" . $this->open_minute : $this->open_minute),
+            'close_time' => (($this->close_hour) < 10 ? "0" . $this->close_hour : $this->close_hour) . ":" . (($this->close_minute) < 10 ? "0" . $this->close_minute : $this->close_minute),
             'features' => $this->getFeatures()
         ];
     }
