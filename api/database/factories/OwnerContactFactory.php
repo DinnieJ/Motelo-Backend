d0ddcaabc97c085bad10db3edfeb6331223ad82b
Hiếu Bùi <buihieu028@gmail.com>
2021-02-25 16:54:17 +0700

    update/seeder
    Loại bỏ trường id , created_at và updated_at

--- api/database/factories/OwnerContactFactory.php
+++ api/database/factories/OwnerContactFactory.php
@@ -8,11 +8,8 @@ use Faker\Generator as Faker;
 $factory->define(\App\Models\OwnerContact::class, function (Faker $faker) {
     return [
         //
-        'id' => $faker->unique()->numberBetween(1, 30),
         'owner_id' => \App\Models\Owner::all()->random()->id,
         'contact_type_id' => \App\Models\MstContactType::all()->random()->id,
-        'content' => $faker->text,
-        'created_at' => \Carbon\Carbon::now()->format('Y-m-d H:i:s'),
-        'updated_at' => \Carbon\Carbon::now()->format('Y-m-d H:i:s')
+        'content' => $faker->text
     ];
 });
--- api/database/factories/OwnerFactory.php
+++ api/database/factories/OwnerFactory.php
@@ -8,7 +8,6 @@ use Faker\Generator as Faker;
 $factory->define(\App\Models\Owner::class, function (Faker $faker) {
     return [
         //
-        'id' => $faker->unique()->numberBetween(1, 30),
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => bcrypt('abcxyz'),
@@ -16,9 +15,5 @@ $factory->define(\App\Models\Owner::class, function (Faker $faker) {
         'date_of_birth' => $faker->date('Y-m-d'),
         'enabled' => 1,
         'remember_token' => null,
-        'created_at' => \Carbon\Carbon::now()->format('Y-m-d H:i:s'),
-        'updated_at' => \Carbon\Carbon::now()->format('Y-m-d H:i:s')
-
-
     ];
 });
