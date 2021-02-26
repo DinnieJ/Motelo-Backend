2c39f9d3b624880fe033311b6ad3c3624a591c8a
Hiếu Bùi <buihieu028@gmail.com>
2021-02-26 11:16:06 +0700

    Update InnDetailForRoomResource.php
    Chỉ lấy id của feature

--- api/app/Http/Resources/InnDetailForRoomResource.php
+++ api/app/Http/Resources/InnDetailForRoomResource.php
@@ -25,7 +25,7 @@ class InnDetailForRoomResource extends JsonResource
             'electric_price' => $this->electric_price,
             'open_time' => (($this->open_hour) < 10 ? "0" . $this->open_hour : $this->open_hour) . ":" . (($this->open_minute) < 10 ? "0" . $this->open_minute : $this->open_minute),
             'close_time' => (($this->close_hour) < 10 ? "0" . $this->close_hour : $this->close_hour) . ":" . (($this->close_minute) < 10 ? "0" . $this->close_minute : $this->close_minute),
-            'features' => $this->getFeatures()
+            'features' => $this->features->pluck('inn_feature_id')
         ];
     }
 
@@ -34,15 +34,6 @@ class InnDetailForRoomResource extends JsonResource
         $data = array_map(function ($value) {
             return new ContactDetailForRoomResource($value);
         }, $this->owner->contacts->toArray());
-
-        return $data;
-    }
-
-    private function getFeatures()
-    {
-        $data = array_map(function ($value) {
-            return new FeaturesDetailForRoomResource($value);
-        }, $this->features->toArray());
 
         return $data;
     }
