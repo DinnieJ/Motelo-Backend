91f4769412042291e2ab7da014b5c3616284880e
Hiếu Bùi <buihieu028@gmail.com>
2021-02-26 09:12:43 +0700

    Update RoomComment.php
    Get thêm tenant_name ứng với mỗi RoomComment
    
    Casts lại created_at và updated_at sang dạng d-m-Y

--- api/app/Models/RoomComment.php
+++ api/app/Models/RoomComment.php
@@ -8,12 +8,26 @@ class RoomComment extends Model
 {
     //
     protected $table = 'tb_room_comment';
+    protected $casts = [
+
+        'created_at' => 'datetime:d-m-Y',
+        'updated_at' => 'datetime:d-m-Y'
+    ];
     protected $fillable = [
         'tenant_id', 'room_id'
     ];
 
+    protected $appends = ['tenant_name'];
+
     public function room()
     {
         $this->belongsTo(Room::class, 'room_id');
+    }
+
+    public function getTenantNameAttribute()
+    {
+        $tenantInfo = $this->belongsTo(Tenant::class, 'tenant_id', 'id')->first();
+        return $tenantInfo->name;
+
     }
 }
