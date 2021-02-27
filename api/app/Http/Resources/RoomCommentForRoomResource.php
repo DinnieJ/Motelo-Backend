290345849f11239b8d37bcf82015493f4b120fbd
Hiếu Bùi <buihieu028@gmail.com>
2021-02-27 10:54:44 +0700

    Update RoomCommentForRoomResource.php
    Bỏ đi những function thừa

--- api/app/Http/Resources/RoomCommentForRoomResource.php
+++ api/app/Http/Resources/RoomCommentForRoomResource.php
@@ -3,7 +3,6 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
-use function Symfony\Component\Translation\t;
 
 class RoomCommentForRoomResource extends JsonResource
 {
