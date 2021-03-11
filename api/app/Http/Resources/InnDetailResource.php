cff1109bed7b415f9a1cfe251b0ea06e90eec91d
Hiếu Bùi <buihieu028@gmail.com>
2021-03-11 23:36:46 +0700

    Update InnDetailResource.php
    Sửa lại InnDetailResource

--- api/app/Http/Resources/InnDetailResource.php
+++ api/app/Http/Resources/InnDetailResource.php
@@ -15,22 +15,21 @@ class InnDetailResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this['id'],
-            'name' => $this['name'],
-            'owner_id' => $this['owner_id'],
-            'water_price' => $this['water_price'],
-            'electric_price' => $this['electric_price'],
-            'open_time' => $this['open_hour'] . ":" . $this['open_minute'],
-            'close_time' => $this['close_hour'] . ":" . $this['close_minute'],
-            'description' => $this['description'],
-            'address' => $this['address'],
-            'location' => $this['latitude'] . " " . $this['longitude'],
-            'status' => $this['status'],
-            'rooms' => $this['rooms'],
-            'features' => array_column($this['features'], 'inn_feature_id'),
-            'created_at' => $this['created_at'],
-            'updated_at' => $this['updated_at']
-//            'created_at' => $this->created_at->format('d . m . Y')
+            'id' => $this->id,
+            'name' => $this->name,
+            'owner_id' => $this->owner_id,
+            'water_price' => $this->water_price,
+            'electric_price' => $this->electric_price,
+            'open_time' => $this->open_hour . ":" . $this->open_minute,
+            'close_time' => $this->close_hour . ":" . $this->close_minute,
+            'description' => $this->description,
+            'address' => $this->address,
+            'location' => $this->latitude . " " . $this->longitude,
+            'status' => $this->status,
+            'rooms' => $this->rooms,
+            'features' => array_column($this->features->toArray(),'inn_feature_id'),
+            'created_at' => $this->created_at->format('d-m-Y'),
+            'updated_at' => $this->updated_at->format('d-m-Y')
         ];
     }
 }
