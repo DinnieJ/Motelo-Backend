5dd57d859457c4b5671ec6e93dc2b5bd0c00fa0f
Hiếu Bùi <buihieu028@gmail.com>
2021-03-13 16:41:13 +0700

    Update InnController.php
    Tách hàm updateImages() và check empty khi so sánh ở hàm diff

--- api/app/Http/Controllers/Inn/InnController.php
+++ api/app/Http/Controllers/Inn/InnController.php
@@ -164,27 +164,16 @@ class InnController extends BaseController
             ], $inn_id);
             // update inn features
             $this->updateFeatures($inn_data['features'], $inn_id);
-            $new_images = $request->file('images');
+            $this->updateImages($request->file('images'), $inn_id);
 
-            $old_images = $this->innImageRepository->where([
-                'inn_id' => $inn_id
-            ])->pluck('filename');
-
-
-            foreach ($new_images as $image) {
-                if (!Storage::disk('s3')->exists("/inns/{$inn_id}/{$image->getClientOriginalName()}")) {
-                    $uploadImg = Storage::disk('s3')->put("/inns/{$inn_id}", $image);
-                    $s3FileName = $this->getS3Filename($uploadImg);
-                    $this->innImageRepository->create([
-                        'inn_id' => $inn_id,
-                        'image_url' => Config::get('filesystems.s3_folder_path') . $uploadImg,
-                        'filename' => $s3FileName
-                    ]);
-                }
-            }
-
+            return response()->json([
+                'message' => "Cập nhật nhà trọ thành công",
+            ], 200);
 
         }
+        return response()->json([
+            'message' => 'Không tìm thấy nhà trọ'
+        ], 404);
 
 
     }
@@ -196,29 +185,81 @@ class InnController extends BaseController
 
         $diff_new_vs_old = array_diff($new_features, $old_features);
         $diff_old_vs_new = array_diff($old_features, $new_features);
-
-
-        foreach ($diff_new_vs_old as $value) {
-            //add new features
-            if (!in_array($value, $old_features)) {
-                $this->innFeatureRepository->create([
-                    'inn_id' => $inn_id,
-                    'inn_feature_id' => $value
-                ]);
+        
+        if (!empty($diff_new_vs_old)) {
+            foreach ($diff_new_vs_old as $value) {
+                //add new features
+                if (!in_array($value, $old_features)) {
+                    $this->innFeatureRepository->create([
+                        'inn_id' => $inn_id,
+                        'inn_feature_id' => $value
+                    ]);
+                }
             }
         }
-        foreach ($diff_old_vs_new as $value) {
-            if (!in_array($value, $new_features)) {
-                $feature = $this->innFeatureRepository->where([
-                    'inn_id' => $inn_id,
-                    'inn_feature_id' => $value
-                ])->delete();
+
+        if (!empty($diff_old_vs_new)) {
+            foreach ($diff_old_vs_new as $value) {
+                if (!in_array($value, $new_features)) {
+                    //delete features
+                    $feature = $this->innFeatureRepository->where([
+                        'inn_id' => $inn_id,
+                        'inn_feature_id' => $value
+                    ])->delete();
+                }
             }
         }
     }
 
-    public function updateImages($inn_id)
+    public function updateImages($new_images, $inn_id)
     {
+        $old_images = $this->innImageRepository->where([
+            'inn_id' => $inn_id
+        ])->pluck('filename')->toArray();
+
+
+        $diff_new_vs_old = array_diff($new_images, $old_images);
+        $diff_old_vs_new = array_diff($old_images, $new_images);
+
+
+        //add new images
+        if (!empty($diff_new_vs_old)) {
+            foreach ($diff_new_vs_old as $value) {
+                if (!in_array($value, $old_images)) {
+                    try {
+                        $uploadImg = Storage::disk('s3')->put("/inns/{$inn_id}", $value);
+                        $s3FileName = $this->getS3Filename($uploadImg);
+                        $this->innImageRepository->create([
+                            'inn_id' => $inn_id,
+                            'image_url' => Config::get('filesystems.s3_folder_path') . $uploadImg,
+                            'filename' => $s3FileName
+                        ]);
+                    } catch (\Exception $e) {
+                        return $e->getMessage();
+                    }
+
+                }
+            }
+        }
+
+
+        //detele images
+        if (!empty($diff_old_vs_new)) {
+            foreach ($diff_old_vs_new as $value) {
+                if (!in_array($value, $new_images)) {
+                    try {
+                        $uploadImg = Storage::disk('s3')->delete("/inns/{$inn_id}/{$value}");
+                        $this->innImageRepository->where([
+                            'inn_id' => $inn_id,
+                            'filename' => $value
+                        ])->delete();
+                    } catch (\Exception $e) {
+                        return $e->getMessage();
+                    }
+
+                }
+            }
+        }
 
 
     }
