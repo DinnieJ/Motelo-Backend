c3297f89947cb42f06771b43de5cd7b30093f1ac
Hiếu Bùi <buihieu028@gmail.com>
2021-03-15 20:22:41 +0700

    add/RoomImage module
    Bổ sung model RoomImage cùng với migration cho tb_room_image
    
    Bổ sung RoomImageRepository + Interface

---
+++ api/database/migrations/2021_03_14_152212_create_room_images_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_room_image', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('room_id');
+            $table->foreign('room_id')->references('id')->on('tb_room')->cascadeOnDelete();
+            $table->string('image_url');
+            $table->string('filename');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_room_image');
+    }
+}
