69c2175c4f8415df908b8abdb3bcdfc22416f9ed
DatNN <datnn288@gmail.com>
2021-04-27 09:20:17 +0700

    truncate db

--- api/database/seeds/DatabaseSeeder.php
+++ api/database/seeds/DatabaseSeeder.php
@@ -12,26 +12,26 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         ///for owner
-        $this->call(OwnerTableSeeder::class);
+        //$this->call(OwnerTableSeeder::class);
         $this->call(MstContactTypeSeeder::class);
-        $this->call(OwnerContactTableSeeder::class);
+        //$this->call(OwnerContactTableSeeder::class);
 
         //for inn
-        $this->call(InnTableSeeder::class);
+        //$this->call(InnTableSeeder::class);
         $this->call(MstFeatureTypeTableSeeder::class);
-        $this->call(InnFeatureTableSeeder::class);
+        //$this->call(InnFeatureTableSeeder::class);
 
         //for tenant
-        $this->call(TenantTableSeeder::class);
+        //$this->call(TenantTableSeeder::class);
 
 
         //for room
         $this->call(MstRoomTypeTableSeeder::class);
         $this->call(MstGenderTypeSeeder::class);
-        $this->call(RoomTableSeeder::class);
-        $this->call(RoomCommentTableSeeder::class);
+        //$this->call(RoomTableSeeder::class);
+        //$this->call(RoomCommentTableSeeder::class);
 
-        $this->call(CollaboratorSeeder::class);
+        //$this->call(CollaboratorSeeder::class);
         $this->call(MstUtilityTypeSeeder::class);
     }
 }
